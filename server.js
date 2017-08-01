5ed933f5924b695dae80e180a28045902c2853de
Ivan <fl125@yandex.ru>
2017-08-02 00:42:09 +0300

    Added compression

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const config = require('./webpack/webpack.config.js');
 const webpackMiddleware = require("webpack-dev-middleware");
 const app = express();
 const compiler = webpack(config);
+const compression = require('compression');
+
 
 const port = process.env.PORT || 3000;
+
+app.use(compression());
 
 app.use(webpackMiddleware(compiler, {
     publicPath : config.output.publicPath,
