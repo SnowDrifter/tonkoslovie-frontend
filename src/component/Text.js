277d31ba35025b50003d0afa7879ed58498c0ab5
Ivan <fl125@yandex.ru>
2017-09-24 01:18:39 +0300

    Refactoring text component

--- src/component/Text.js
+++ src/component/Text.js
@@ -18,10 +18,10 @@ import {
     Col
 } from "react-bootstrap";
 import client from "../util/client";
-import * as partTypes from  './TextPartTypes'
-import styles from './Text.less';
+import style from './Text.less'
+import * as partTypes from "./TextPartTypes";
 import Helmet from "react-helmet";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 class LessonText extends React.Component {
 
@@ -69,35 +69,12 @@ class LessonText extends React.Component {
         let textParts = this.state.textParts;
 
         textParts.map((part, index) => {
-            if (part.type == partTypes.QUESTION) {
-                let answer = ReactDOM.findDOMNode(this['form-' + index]).value;
-                answer = answer.trim().toLowerCase();
-
-                if (answer == part.data.toLowerCase()) {
-                    part.success = true;
-                } else {
-                    part.error = true;
-                }
-            } if (part.type == partTypes.CHOICE) {
-                let answer = ReactDOM.findDOMNode(this['form-' + index]).value;
-
-                if(this.checkChoiceVariant(answer, part.choiceVariants)) {
-                    part.success = true;
-                } else {
-                    part.error = true;
-                }
+            if (part.type == partTypes.QUESTION || part.type == partTypes.CHOICE) {
+                this['part-' + index].checkAnswer();
             }
         });
 
         this.setState({textParts: textParts});
-    }
-
-    checkChoiceVariant(currentVariant, variants) {
-       return variants.some(value => {
-            if(value.title == currentVariant) {
-                return !!value.right;
-            }
-        });
     }
 
     render() {
@@ -106,66 +83,24 @@ class LessonText extends React.Component {
 
         this.state.textParts.map((part, index) => {
             switch (part.type) {
-                case partTypes.TEXT:
+                case partTypes.TEXT: {
                     components.push(<dev className="text-part" key={index}>{part.data}</dev>);
                     break;
-                case partTypes.LINE_BREAK:
+                }
+                case partTypes.LINE_BREAK: {
                     components.push(<div className="clearfix" key={index}/>);
                     break;
+                }
                 case partTypes.QUESTION: {
-                    let validateState;
-                    let disabled = false;
-                    if (part.success) {
-                        validateState = "success";
-                        disabled = true;
-                    } else if (part.error) {
-                        validateState = "error";
-                    }
-
-                    components.push(<FormGroup key={index} validationState={validateState} className="text-part">
-                        <FormControl
-                            ref={part => {
-                                this['form-' + index] = part
-                            }}
-                            style={{width: part.data.length * 15 + 15}}
-                            type="text"
-                            bsSize="small"
-                            disabled={disabled}
-                            placeholder={part.placeholder}
-                            maxLength={part.data.length}
-                        />
-                        <FormControl.Feedback />
-                    </FormGroup>);
+                    components.push(<QuestionPart ref={instance => {
+                        this['part-' + index] = instance;
+                    }} key={index} part={part} index={index}/>);
                     break;
                 }
                 case partTypes.CHOICE: {
-                    const variants = [];
-                    variants.push(<option key={-1} value="-1">Выберите правильный вариант</option>);
-
-                    part.choiceVariants.map((value, index) => {
-                        variants.push(<option key={index} value={value.title}>{value.title}</option>);
-                    });
-
-                    let validateState;
-                    let disabled = false;
-                    if (part.success) {
-                        validateState = "success";
-                        disabled = true;
-                    } else if (part.error) {
-                        validateState = "error"
-                    }
-
-                    components.push( <FormGroup  validationState={validateState} key={index}>
-                        <FormControl componentClass="select"
-                                     placeholder="select"
-                                     bsSize="small"
-                                     disabled={disabled}
-                                     ref={part => {
-                                         this['form-' + index] = part
-                                     }}>
-                            {variants}
-                        </FormControl>
-                    </FormGroup>);
+                    components.push(<ChoicePart ref={instance => {
+                        this['part-' + index] = instance;
+                    }} part={part} key={index} index={index}/>);
                     break;
                 }
             }
@@ -204,6 +139,103 @@ class LessonText extends React.Component {
         } else {
             return null;
         }
+    }
+}
+
+class QuestionPart extends React.Component {
+
+    checkAnswer() {
+        const part = this.props.part;
+        let answer = ReactDOM.findDOMNode(this['form-' + this.props.index]).value;
+        answer = answer.trim().toLowerCase();
+
+        if (answer == part.data.toLowerCase()) {
+            part.success = true;
+        } else {
+            part.error = true;
+        }
+    }
+
+    render() {
+        const part = this.props.part;
+        let validateState;
+        let disabled = false;
+
+        if (part.success) {
+            validateState = "success";
+            disabled = true;
+        } else if (part.error) {
+            validateState = "error";
+        }
+
+        return <FormGroup validationState={validateState} className="text-part">
+            <FormControl
+                ref={part => {
+                    this['form-' + this.props.index] = part
+                }}
+                style={{width: part.data.length * 15 + 15}}
+                type="text"
+                bsSize="small"
+                disabled={disabled}
+                placeholder={part.placeholder}
+                maxLength={part.data.length}
+            />
+            <FormControl.Feedback />
+        </FormGroup>;
+    }
+}
+
+class ChoicePart extends React.Component {
+
+    checkAnswer() {
+        const part = this.props.part;
+        let answer = ReactDOM.findDOMNode(this['form-' + this.props.index]).value;
+
+        if (this.checkChoiceVariant(answer, part.choiceVariants)) {
+            part.success = true;
+            return true;
+        } else {
+            part.error = true;
+            return false;
+        }
+    }
+
+    checkChoiceVariant(currentVariant, variants) {
+        return variants.some(value => {
+            if (value.title == currentVariant) {
+                return !!value.right;
+            }
+        });
+    }
+
+    render() {
+        const part = this.props.part;
+        const variants = [];
+        variants.push(<option key={-1} value="-1">Выберите правильный вариант</option>);
+
+        part.choiceVariants.map((value, index) => {
+            variants.push(<option key={index} value={value.title}>{value.title}</option>);
+        });
+
+        let validateState;
+        let disabled = false;
+        if (part.success) {
+            validateState = "success";
+            disabled = true;
+        } else if (part.error) {
+            validateState = "error"
+        }
+
+        return <FormGroup validationState={validateState}>
+            <FormControl componentClass="select"
+                         bsSize="small"
+                         disabled={disabled}
+                         ref={part => {
+                             this['form-' + this.props.index] = part
+                         }}>
+                {variants}
+            </FormControl>
+        </FormGroup>
     }
 }
 
