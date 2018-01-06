b02dbd7e606f4cd92aa85fd608ef3b7b08bfbafc
Ivan <fl125@yandex.ru>
2018-01-06 03:02:53 +0300

    Refactoring

--- src/util/client.js
+++ src/util/client.js
@@ -20,14 +20,12 @@ let client;
     client.interceptors.response.use(null, function (error) {
         let response = error.response;
 
-        if(error.message == "Network Error") {
+        if(error.message == "Network Error" || response.status >= 500) {
             browserHistory.push("/500");
         } else if (response.status === 403) {
             localStorage.removeItem("token");
             window.location.reload();
             browserHistory.push("/");
-        } else if (response.status >= 500) {
-            browserHistory.push("/500");
         }
 
         return Promise.reject(error);
