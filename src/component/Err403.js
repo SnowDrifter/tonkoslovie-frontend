e78b80bd51761f726943d254a4881f7dd30a9c46
Ivan <fl125@yandex.ru>
2018-01-07 03:57:06 +0300

    Update error pages

--- src/component/Err403.js
+++ src/component/Err403.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {Jumbotron} from "react-bootstrap";
+import "./Error.less"
 
 const Er403 = () => {
-    return <Jumbotron><h2 style={{color: "red", textAlign: "center"}}>Доступ запрещен</h2></Jumbotron>
+    return <Jumbotron><h2 className="error-message">Доступ запрещен</h2></Jumbotron>
 };
 
 export default Er403;
--- src/component/Err500.js
+++ src/component/Err500.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {Jumbotron} from "react-bootstrap";
+import "./Error.less"
 
 const Err500 = () => {
     return <Jumbotron>
-        <h3 style={{color: "red", textAlign: "center"}}>На сервере проводятся технические работы</h3>
-        <h4 style={{color: "red", textAlign: "center"}}>Приносим извинения за неудобства</h4>
+        <h3 className="error-message">На сервере проводятся технические работы</h3>
+        <h4 className="error-message">Приносим извинения за неудобства</h4>
     </Jumbotron>
 };
 
