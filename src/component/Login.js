5fb9684e6c2211869ace43fdc9087dc6e2f26b68
Ivan <fl125@yandex.ru>
2018-06-06 03:45:35 +0300

    Updated login modal dialog

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
-import {FormGroup, Row, Col, ControlLabel, FormControl, Button, Modal} from "react-bootstrap";
+import {Button, ControlLabel, FormControl, FormGroup, Modal} from "react-bootstrap";
 import * as UserActions from "../action/Auth";
 import "./Login.less";
 import Oauth from "./Oauth";
@@ -34,39 +34,31 @@ class Login extends React.Component {
         let title = "Вход";
 
         return <div>
-            <Modal show={this.state.showLogin} onHide={this.props.actions.hideLogin}>
+            <Modal show={this.state.showLogin} onHide={this.props.actions.hideLogin} dialogClassName="login-modal">
                 <Modal.Header closeButton>
                     <Modal.Title>{title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body className="login-modal-body">
                     <form className="login-form">
                         <FormGroup>
-                            <Row>
-                                <Col md={12}>
-                                    <FormGroup controlId="emailForm">
-                                        <ControlLabel>Email</ControlLabel>
-                                        <FormControl ref={email => {
-                                            this.email = email
-                                        }} type="text" autoFocus/>
-                                    </FormGroup>
-                                </Col>
-                            </Row>
+                            <FormGroup controlId="emailForm">
+                                <ControlLabel>Email</ControlLabel>
+                                <FormControl ref={email => {
+                                    this.email = email
+                                }} type="text" autoFocus/>
+                            </FormGroup>
 
-                            <Row>
-                                <Col md={12}>
-                                    <FormGroup controlId="passwordForm">
-                                        <ControlLabel>Пароль</ControlLabel>
-                                        <FormControl ref={password => {
-                                            this.password = password
-                                        }} type="password"/>
-                                    </FormGroup>
-                                </Col>
-                            </Row>
+                            <FormGroup controlId="passwordForm">
+                                <ControlLabel>Пароль</ControlLabel>
+                                <FormControl ref={password => {
+                                    this.password = password
+                                }} type="password"/>
+                            </FormGroup>
                         </FormGroup>
 
-                        <div className="login-error-message">{this.props.user.errorMessage}</div>
+                        <div className="login-error-message text-center">{this.props.user.errorMessage}</div>
 
-                        <Button type="submit" className="pull-right" bsStyle="success"
+                        <Button type="submit" className="center-block" bsStyle="success"
                                 onClick={this.sendLogin.bind(this)}>Войти</Button>
                     </form>
 
