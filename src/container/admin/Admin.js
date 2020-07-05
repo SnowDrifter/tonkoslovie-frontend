778eff8edb04bdcbc234da93a97b8f750fa50191
Ivan <fl125@yandex.ru>
2020-07-05 19:43:41 +0300

    Update admin page

--- src/container/admin/Admin.js
+++ src/container/admin/Admin.js
@@ -1,23 +1,22 @@
 import React from "react";
-import {Link} from "react-router";
-import {PageHeader, Panel} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {Card} from "react-bootstrap";
 
 class Admin extends React.Component {
 
     render() {
-        return <Panel>
-            <Panel.Body>
-                <PageHeader className="text-center">Админка</PageHeader>
-
-                <Link to="/admin/lessons" activeClassName="active"><h3>Уроки</h3></Link>
-                <Link to="/admin/texts" activeClassName="active"><h3>Тексты</h3></Link>
-                <Link to="/admin/words" activeClassName="active"><h3>Слова</h3></Link>
-                <Link to="/admin/exercises" activeClassName="active"><h3>Упражнения</h3></Link>
-                <Link to="/admin/themes" activeClassName="active"><h3>Темы упражнений</h3></Link>
+        return <Card>
+            <Card.Header className="text-center"><h2>Админка</h2></Card.Header>
+            <Card.Body>
+                <Link to="/admin/lessons"><h3>Уроки</h3></Link>
+                <Link to="/admin/texts"><h3>Тексты</h3></Link>
+                <Link to="/admin/words"><h3>Слова</h3></Link>
+                <Link to="/admin/exercises"><h3>Упражнения</h3></Link>
+                <Link to="/admin/themes"><h3>Темы упражнений</h3></Link>
                 <hr/>
-                <Link to="/admin/users" activeClassName="active"><h3>Пользователи</h3></Link>
-            </Panel.Body>
-        </Panel>
+                <Link to="/admin/users"><h3>Пользователи</h3></Link>
+            </Card.Body>
+        </Card>
     }
 }
 
