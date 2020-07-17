59efb623feb7bbd90f3864d00bffed9c2aa03d77
Ivan <fl125@yandex.ru>
2020-07-17 21:36:53 +0300

    Remove button sizes

--- src/component/admin/EditRemoveButtons.js
+++ src/component/admin/EditRemoveButtons.js
@@ -6,13 +6,11 @@ class EditRemoveButtons extends React.Component {
     render() {
         return <>
             <ButtonGroup className="button-block">
-                <Button size="xsmall"
-                        style={{padding: "0.3rem 0.4rem"}}
+                <Button style={{padding: "0.3rem 0.4rem"}}
                         onClick={this.props.edit}>
                     <BsPencil size={18}/>
                 </Button>
-                <Button size="xsmall"
-                        variant="danger"
+                <Button variant="danger"
                         style={{padding: "0.1rem 0.2rem"}}
                         onClick={this.props.remove}>
                     <BsX size={24}/>
--- src/component/admin/RemoveButton.js
+++ src/component/admin/RemoveButton.js
@@ -5,8 +5,7 @@ import {BsX} from "react-icons/bs";
 class RemoveButton extends React.Component {
     render() {
         return  <>
-            <Button size="xsmall"
-                    variant="danger"
+            <Button variant="danger"
                     style={{padding: "0.1rem 0.2rem"}}
                     onClick={this.props.remove}>
                 <BsX size={24}/>
