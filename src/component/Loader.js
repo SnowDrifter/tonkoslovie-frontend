06c32fac5aa03e46f9d390eca27abbd230ca910e
Ivan Romanov <fl125@yandex.ru>
2021-05-15 18:03:13 +0300

    Change loader, remove halogenium

--- src/component/Loader.js
+++ src/component/Loader.js
@@ -1,12 +1,13 @@
 import React from "react";
-import {ScaleLoader} from "halogenium";
+import Spinner from "react-loader-spinner";
 import "./Loader.less"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 class Loader extends React.Component {
 
     render() {
         return <div className="loader">
-            <ScaleLoader color="#b1b1b1"/>
+            <Spinner type="Bars" color="#b1b1b1"/>
         </div>;
     }
 }
