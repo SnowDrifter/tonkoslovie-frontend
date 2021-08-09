0a85736ca1e49c1b3aebedb9a774ec571a2ba5e7
Ivan Romanov <fl125@yandex.ru>
2021-08-09 22:47:00 +0300

    Redesign of the confirmation modal

--- src/component/SimpleConfirmModal.js
+++ src/component/SimpleConfirmModal.js
@@ -3,7 +3,7 @@ import {Button, Modal} from "react-bootstrap";
 
 class SimpleConfirmModal extends React.Component {
     render() {
-        return <Modal show={this.props.showModal} onHide={this.props.hideModal.bind(this)}>
+        return <Modal show={this.props.showModal} onHide={this.props.hideModal.bind(this)} centered>
             <Modal.Header closeButton>
                 <Modal.Title>Подтверждение</Modal.Title>
             </Modal.Header>
@@ -11,9 +11,9 @@ class SimpleConfirmModal extends React.Component {
                 <h4 className="text-center">{this.props.modalTitle}</h4>
 
                 <div className="text-center">
-                    <Button onClick={() => this.props.confirmFunction(true)} variant="success" size="lg">Да</Button>
+                    <Button onClick={() => this.props.confirmFunction(true)}>Да</Button>
                     {" "}
-                    <Button onClick={() => this.props.negativeFunction()} size="lg">Нет</Button>
+                    <Button onClick={() => this.props.negativeFunction()} variant="danger">Нет</Button>
                 </div>
             </Modal.Body>
         </Modal>
